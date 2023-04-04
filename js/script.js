6a7201a855de79dca48ef05cbac62d698357075a
Kizenko <vlad.helpbox@gmail.com>
2023-04-04 07:16:05 +0200

    update

--- js/script.js
+++ js/script.js
@@ -73,13 +73,81 @@
 // const clients = ["Mango", "Ajax", "Poly", "Kiwi"];
 // console.log(clients.slice(1, 3));
 
-const scores = [1, 2, 3, 4, 5];
+// const scores = [1, 2, 3, 4, 5];
 
 // Видаляємо три елементи масиву, починаючи з першого елемента (індекс 0)
-const deletedScores = scores.splice(0, 3);
+// const deletedScores = scores.splice(0, 3);
 
 // Тепер масив scores містить два елементи
-console.log(scores); // [4, 5]
+// console.log(scores); // [4, 5]
 
 // А масив deletedScores містить три видалені елементи
-console.log(deletedScores); // [1, 2, 3]
+// console.log(deletedScores); // [1, 2, 3]
+
+// function multiply(x, y, z) {
+//   console.log("before return");
+//   return x * y * z;
+//   console.log("after return");
+// }
+// let result = multiply(2, 3, 5);
+// console.log(result);
+
+// result = multiply(4, 8, 12);
+// console.log(result);
+// result = multiply(17, 6, 25);
+// console.log(result);
+
+// const add = function (a, b) {
+//   console.log(a);
+//   console.log(b);
+//   return a + b;
+// };
+// let result = add(4, 3);
+// console.log("result", result);
+
+// let result1 = add(5, 6);
+// console.log("result", result1);
+
+// let result2 = add(6, 7);
+// console.log("result", result2);
+
+// const sum = function (a, b, c) {
+//   console.log(a);
+//   console.log(b);
+//   console.log(c);
+//   return a + b + c;
+// };
+// const result = sum(11, 22, 33);
+// console.log("result:", result);
+
+// const salary = function (a, b) {
+//   if (a <= b) {
+//     return "A менше за B";
+//   }
+//   return "A біьше за B";
+// };
+// let price = 10;
+// let wallet = 20;
+// console.log(salary(price, wallet));
+
+const fnA = function () {
+  console.log("is perfomed A");
+  fnB();
+};
+const fnB = function () {
+  console.log("is perfomed B");
+  fnC();
+};
+const fnC = function () {
+  console.log("is perfomed C");
+  fnD();
+};
+const fnD = function () {
+  console.log("is perfomed D");
+  fnE();
+};
+const fnE = function () {
+  console.log("is perfomed E");
+};
+
+fnA();
